5aa8526387645a8aaa3a64a0cbf4806d3c9561ce
Yatin Kanetkar <yatin@pindropsecurity.com>
2016-12-20 16:44:43 -0500

    Added some examples for pattern matching

--- src/useless.erl
+++ src/useless.erl
@@ -1,5 +1,5 @@
 -module(useless).
--export([hello/0, add/2, greet_and_add_two/1]).
+-export([hello/0, add/2, greet_and_add_two/1, same/2, valid_time/1, correct_age/1, wrong_age/1]).
 -import(io, [format/1]).
 
 hello() ->
@@ -13,3 +13,24 @@ greet_and_add_two(X) ->
     add(X,2).
 
 -define(sub(X,Y), add(X, -Y)).
+
+% Pattern Matching
+same(A,A) ->
+    true;
+same(_,_) ->
+    false.
+
+valid_time({Date = {_,_,_}, Time = {_,_,_}}) ->
+    io:format("The Date is (~p) and the time is ~p ~n", [Date, Time]);
+valid_time(_) ->
+    io:format("Give me some good data yo!~n").
+
+correct_age(X) when X >= 16, X =< 104 ->
+    true;
+correct_age(_) ->
+    false.
+
+wrong_age(X) when X < 16; X > 104 ->
+    true;
+wrong_age(_) ->
+    false.
