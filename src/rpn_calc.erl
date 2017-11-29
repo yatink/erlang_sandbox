42e647b6b5b9f4d676c1f808d53f591d7bb1cb68
Yatin Kanetkar <ykanetkar@africangrey.ad.pdrop.net>
2017-11-29 10:55:08 -0500

    Make rpn_agg more functional

--- src/rpn_calc.erl
+++ src/rpn_calc.erl
@@ -1,19 +1,27 @@
 -module(rpn_calc).
--export([rpn_agg/2, rpn/1]).
+-export([rpn_agg/2, rpn/1, rpn_test/0]).
 
-% rpn (Token, Accumulator stack) -> new accumulator stack 
-rpn_agg(Acc, Token) -> 
-    try {list_to_integer(Token), Acc} of
-        {Val, Acc} -> [Val|Acc]
-     catch
-         error:badarg ->
-             case {Token, Acc} of
-                 {"+", [Elem1|[Elem2|C]]} -> [Elem1 + Elem2 | C];
-                 {"-", [Elem1|[Elem2|C]]} -> [Elem1 - Elem2 | C];
-                 {"*", [Elem1|[Elem2|C]]} -> [Elem1 * Elem2 | C];
-                 {"/", [Elem1|[Elem2|C]]} -> [Elem1 div Elem2 | C]
-             end
-     end.
-                     
+% rpn_agg (Token, AccIn) -> AccOut
+
+rpn_agg("+", [Elem1|[Elem2|C]]) -> [Elem2 + Elem1 | C];
+rpn_agg("-", [Elem1|[Elem2|C]]) -> [Elem2 - Elem1 | C];
+rpn_agg("*", [Elem1|[Elem2|C]]) -> [Elem2 * Elem1 | C];
+rpn_agg("/", [Elem1|[Elem2|C]]) -> [Elem2 div Elem1 | C];
+rpn_agg(Token, Acc) -> [list_to_integer(Token)|Acc].
+    
+
 rpn(Expression) ->
-    lists:foldl(fun rpn_agg/2, [], string:tokens(Expression, " ")).
+    [Res] = lists:foldl(fun rpn_agg/2, [], string:tokens(Expression, " ")),
+    erlang:display(Res),
+    Res.
+
+rpn_test() ->
+    5 = rpn("2 3 +"),
+    1 = rpn("3 2 -"),
+    -2 = rpn("10 4 3 + 2 * - 2 /"),
+    ok = try 
+             rpn("90 34 12 33 55 66 + * - +") 
+         catch
+             error:{badmatch, [_|_]} -> ok
+         end.
+        
